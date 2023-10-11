d10d064c0a241d4c50a3c27321ad33b0b8768921
Ivan Khabarov <andre9iguodala@gmail.com>
2023-10-11 18:52:14 +0300

    added example005

---
+++ Folder_1_1/example005_if_else_condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ura eto zhe Masha");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
